fb82d5714d811d6f57f35bc7d75c7f65a4a9aa12
Prajal123 <106102085@nitt.edu>
2021-07-27 13:42:01 +0530

    Hackermode Initial commit

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var ctx = canvas.getContext('2d');
 canvas.height = innerHeight - 22;
 canvas.width = innerWidth - 22;
 
-var missilearray = [];
-// var dx=.1;
+var missilearray = [], alienfall = [];
+
 var timer = 00, count = 0;
 var found = 0;
 var score = 0;
@@ -25,7 +25,6 @@ ctx.fill();
 ctx.stroke();
 ctx.closePath();
 
-var count1 = 0, count2 = 0, level = 0;
 window.addEventListener("keydown", function (e) {
     if (e.key == 'ArrowRight' || e.key == 'ArrowDown') {
         if (e.key == 'ArrowRight' && (a >= -1 && a + 61 <= canvas.width)) {
@@ -81,6 +80,7 @@ window.addEventListener("keydown", function (e) {
 function alien(x, y) {
     this.x = x;
     this.y = y;
+    this.check = 0;
     this.draw = function () {
         ctx.beginPath();
         ctx.moveTo(5 + 5 * this.x, 5 + 5 * this.y);
@@ -135,38 +135,141 @@ function alien(x, y) {
     }
 }
 
-window.addEventListener("keydown", function (e) {
+function alien2(x, y) {
+    this.x = x;
+    this.y = y;
+    this.check = 1;
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.moveTo(5 + 5 * this.x, 5 + 5 * this.y);
+        ctx.lineTo(45 + 5 * this.x, 5 + 5 * this.y);
+        ctx.lineTo(45 + 5 * this.x, 45 + 5 * this.y);
+        ctx.lineTo(25 + 5 * this.x, 5 + 5 * this.y);
+        ctx.lineTo(5 + 5 * this.x, 45 + 5 * this.y);
+        ctx.lineTo(5 + 5 * this.x, 5 + 5 * this.y);
+        ctx.fillStyle = "lime";
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "lime";
+        ctx.fillText("Score :" + score, 10, 350);
+
+        timer += .01;
+        if (timer > 60) {
+            count++; timer = 0;
+            ctx.fillText(count + ":0" + Math.floor(timer), 10, 400);
+        }
+        else if (timer > 10) {
+            ctx.fillText("Timer:" + count + ":" + Math.floor(timer), 10, 400);
+        }
+        else if (timer < 10) {
+            ctx.fillText("Timer:" + count + ":0" + Math.floor(timer), 10, 400);
+        }
+    }
+
+    this.update = function () {
+
+        if (alienarray[0].x + 1245 >= canvas.width) {
+            found = 1;
+            this.x -= 0.1;
+        }
+        else if (alienarray[0].x + 1 <= 0) {
+            found = 0;
+            this.x += .1;
+        }
+        else {
+            if (found == 0) {
+                this.x += .1;
+            }
+            else {
+                this.x -= 0.1;
+            }
+        }
+
+
+        this.draw();
+    }
+}
+window.addEventListener("keydown", spacepress);
+
+function spacepress(e) {
     if (e.key == " ") {
         missilearray.push(new missile(a + 30, b - 50));
         animate1(a + 30, b - 50);
     }
-})
+}
 function missile(x, y) {
     this.x = x;
     this.y = y;
     this.draw1 = function () {
 
-        for (var i = 0; i < alienarray.length; i++) {
-            if (alienarray[i] == null) {
-                return;
-            }
-            if (this.x >= (alienarray[i].x * 5 + 5) && this.x <= (alienarray[i].x * 5 + 45) && this.y == (alienarray[i].y * 5 + 45)) {
-                alienarray.splice(i, 1);
-                missilearray.splice(missilearray.length - 1, 1);
-                score += 10;
+        if (score <= 500) {
+            for (var i = 0; i < alienarray.length; i++) {
+                if (this.x >= (alienarray[i].x * 5 + 5) && this.x <= (alienarray[i].x * 5 + 45) && this.y == (alienarray[i].y * 5 + 45)) {
+                    if (alienarray[i].check == 0) {
+                        alienarray.splice(i, 1);
+                        missilearray.splice(missilearray.length - 1, 1);
+                        score += 10;
+                    }
+                    else if (alienarray[i].check == 1) {
+                        if (count1[i] == 1) {
+                            alienarray.splice(i, 1);
+                            missilearray.splice(missilearray.length - 1, 1);
+                            score += 10;
+                        }
+                        else {
+                            missilearray.splice(missilearray.length - 1, 1);
+                            score += 10;
+                            count1[i] = 1;
+                        }
+                    }
+                }
             }
 
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(this.x, this.y - 30);
+            ctx.strokeStyle = "white";
+            ctx.stroke();
+            ctx.closePath();
+        }
+        else {
+
+            for (var i = 0; i < alienarray.length; i++) {
+                if ((this.x >= (alienarray[i].x * 5 + 5) && this.x <= (alienarray[i].x * 5 + 45) && this.y == (alienarray[i].y * 5 + 45)) || (this.x - 25 >= (alienarray[i].x * 5 + 5) && this.x - 25 <= (alienarray[i].x * 5 + 45) && this.y == (alienarray[i].y * 5 + 45)) || (this.x + 25 >= (alienarray[i].x * 5 + 5) && this.x + 25 <= (alienarray[i].x * 5 + 45) && this.y == (alienarray[i].y * 5 + 45))) {
+                    if (alienarray[i].check == 0) {
+                        alienarray.splice(i, 1);
+                        missilearray.splice(missilearray.length - 1, 1);
+                        score += 10;
+                    }
+                    else if (alienarray[i].check == 1) {
+                        if (count1[i] == 1) {
+                            alienarray.splice(i, 1);
+                            missilearray.splice(missilearray.length - 1, 1);
+                            score += 10;
+                        }
+                        else {
+                            missilearray.splice(missilearray.length - 1, 1);
+                            score += 10;
+                            count1[i] = 1;
+                        }
+                    }
+                }
+            }
+
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(this.x, this.y - 30);
+            ctx.moveTo(this.x - 25, this.y);
+            ctx.lineTo(this.x - 25, this.y - 30);
+            ctx.moveTo(this.x + 25, this.y);
+            ctx.lineTo(this.x + 25, this.y - 30);
+            ctx.strokeStyle = "white";
+            ctx.stroke();
+            ctx.closePath();
 
         }
-
-        ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x, this.y - 30);
-        ctx.strokeStyle = "white";
-        ctx.stroke();
-        ctx.closePath();
-
-
     }
     this.update1 = function () {
         this.y -= 1;
@@ -183,12 +286,19 @@ function animate1() {
     missilearray[missilearray.length - 1].update1();
 
 }
-var alienarray = [];
+var alienarray = [], count1 = [];
 for (var i = 0; i < 240; i += 10) {
     for (var j = 0; j < 60; j += 10) {
-        alienarray.push(new alien(i, j));
+        if (Math.floor(Math.random() * 2) == 0) {
+            alienarray.push(new alien(i, j));
+        }
+        else {
+            alienarray.push(new alien2(i, j));
+            count1.push(0);
+        }
     }
 }
+
 var animation;
 function animate() {
     animation = requestAnimationFrame(animate);
@@ -210,14 +320,13 @@ var animation3;
 
 function fall() {
     var j = Math.floor(Math.random() * alienarray.length);
-    // alienfall.push(alienarray[j]);
+
     this.al = alienarray[j];
     alienarray.splice(j, 1);
-    //  alienfall1=alienfall[alienfall.length-1];
+
     this.update3 = function () {
         this.al.y += 1;
         this.al.draw();
-        //    console.log(this.al.y);
 
         if (((this.al.x * 5 + 45 >= a && this.al.x * 5 <= a) || (this.al.x * 5 <= a + 60 && this.al.x * 5 + 45 >= a + 60)) && this.al.y * 5 >= b - 50 && this.al.y * 5 <= b) {
             cancelAnimationFrame(animation);
@@ -235,8 +344,9 @@ function fall() {
             ctx.fillText("Total score:" + Math.floor(score + 50), canvas.width / 2, canvas.height / 2 + 100);
             clearTimeout(t);
             levelup();
-        }
 
+
+        }
     }
 }
 
@@ -285,7 +395,13 @@ function levelup() {
 
         for (var i = 0; i < 240; i += 10) {
             for (var j = 0; j < 60; j += 10) {
-                alienarray.push(new alien(i, j));
+                if (Math.floor(Math.random() * 2) == 0) {
+                    alienarray.push(new alien(i, j));
+                }
+                else {
+                    alienarray.push(new alien2(i, j));
+                    count1.push(0);
+                }
             }
         }
         var animation4;
@@ -296,7 +412,7 @@ function levelup() {
 
                 alienarray[i].update();
             }
-            ctx.fillText("Level 2", 1100, 320);
+            ctx.fillText("Special Level 2", 1100, 320);
         }
         animate4();
 
@@ -352,7 +468,6 @@ function levelup() {
 
 
 }
-
 function levelup2() {
     setTimeout(function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -375,7 +490,13 @@ function levelup2() {
 
         for (var i = 0; i < 240; i += 10) {
             for (var j = 0; j < 60; j += 10) {
-                alienarray.push(new alien(i, j));
+                if (Math.floor(Math.random() * 2) == 0) {
+                    alienarray.push(new alien(i, j));
+                }
+                else {
+                    alienarray.push(new alien2(i, j));
+                    count1.push(0);
+                }
             }
         }
         var animation6;
@@ -386,7 +507,7 @@ function levelup2() {
 
                 alienarray[i].update();
             }
-            ctx.fillText("Level 3", 1100, 320);
+            ctx.fillText("Special Level 3", 1100, 320);
         }
         animate6();
 
@@ -421,11 +542,8 @@ function levelup2() {
                     cancelAnimationFrame(animation6);
                     cancelAnimationFrame(animation7);
                     highscore1();
-                    ctx.fillText("Congratulations, you won all the levels", canvas.width / 2, canvas.height / 2 + 50);
-                    ctx.fillText("Bonus Points :150 ", canvas.width / 2, canvas.height / 2 + 75);
-                    score += 150;
-                    ctx.fillText("Total score:" + Math.floor(score), canvas.width / 2, canvas.height / 2 + 100);
                     clearTimeout(r);
+                    levelup2b();
                 }
             }
         }
@@ -444,4 +562,128 @@ function levelup2() {
 
 
 }
+function levelup2b() {
+    ctx.clearRect(0, 0, canvas.width, b - 50);
+    var animation8, dx = 1, animation9, counting = 0;
+    var missilearray2 = [];
+    alienarray = [];
+    alienarray.length = 0;
+    alienarray.splice(0, alienarray.length);
+    function bigalien(x, y) {
+        this.x = x;
+        this.y = y;
+        this.draw = function () {
 
+            ctx.beginPath();
+            ctx.moveTo(this.x - 100, this.y - 200);
+            ctx.lineTo(this.x, this.y - 100);
+            ctx.lineTo(this.x + 100, this.y - 200);
+            ctx.fillStyle = "lime";
+            ctx.fill();
+            ctx.closePath();
+        }
+        this.update = function () {
+            if (this.x - 100 <= 0 || this.x + 100 >= canvas.width) {
+                dx = -dx;
+            }
+            this.x += dx;
+            this.draw();
+
+            ctx.fillText("Score:" + score, 10, 350);
+        }
+
+    }
+    var bigalien1 = new bigalien(canvas.width / 2, canvas.height / 2);
+    function animate8() {
+        ctx.clearRect(0, 0, canvas.width, b - 50);
+        animation8 = requestAnimationFrame(animate8);
+        bigalien1.update();
+    }
+    animate8();
+
+    function missile1(x, y) {
+        this.x = x;
+        this.y = y;
+        this.draw = function () {
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(this.x, this.y + 30);
+            ctx.stroke();
+            ctx.strokeStyle = "white";
+            ctx.closePath();
+
+        }
+        this.update = function () {
+            this.y += 2;
+            this.draw();
+
+            if (this.x >= a && this.x <= a + 60 && this.y >= b - 50) {
+                clearTimeout(q);
+                cancelAnimationFrame(animation8);
+                cancelAnimationFrame(animation9);
+                highscore1();
+                ctx.fillText("You lost", canvas.width / 2, canvas.height / 2);
+            }
+        }
+    }
+    var missilefall = new missile1(bigalien1.x, bigalien1.y - 100);
+    var q = setInterval(function () {
+        missilefall = new missile1(bigalien1.x, bigalien1.y - 100);
+    }, 3000);
+    function animate9() {
+        ctx.clearRect(0, b - 50, a, canvas.height - b + 50);
+        ctx.clearRect(a + 60, b - 50, canvas.width - a - 60, canvas.height - b + 50);
+        animation9 = requestAnimationFrame(animate9);
+        missilefall.update();
+    }
+    animate9();
+
+    function missile2(x, y) {
+        this.x = x;
+        this.y = y;
+        this.draw = function () {
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(this.x, this.y - 30);
+            ctx.strokeStyle = "white";
+            ctx.stroke();
+            ctx.closePath();
+        }
+        this.update = function () {
+            this.y -= 1;
+            this.draw();
+            if (this.x >= bigalien1.x - 100 && this.x <= bigalien1.x + 100 && this.y - 30 <= bigalien1.y - 200) {
+                missilearray2.splice(missilearray2.length - 1, 1);
+                counting++;
+                score += 20;
+            }
+            if (counting >= 5) {
+                bigalien1 = null;
+                cancelAnimationFrame(animation8);
+                cancelAnimationFrame(animation9);
+                clearTimeout(q);
+                ctx.clearRect(0, 0, canvas.width, b - 50);
+                highscore1();
+
+                ctx.fillText("Congratulations,you won all the levels", canvas.width / 2, canvas.height / 2 + 50);
+                ctx.fillText("Bonus Points :150 ", canvas.width / 2, canvas.height / 2 + 75);
+                score += 150;
+                ctx.fillText("Your Total score:" + Math.floor(score), canvas.width / 2, canvas.height / 2 + 100);
+            }
+        }
+    }
+
+    window.removeEventListener("keydown", spacepress);
+    window.addEventListener("keydown", function (e) {
+        if (e.key == " ") {
+            missilearray2.push(new missile2(a + 30, b - 50));
+            animate10();
+        }
+    })
+
+    function animate10() {
+        requestAnimationFrame(animate10);
+        missilearray2[missilearray2.length - 1].update();
+    }
+
+}
